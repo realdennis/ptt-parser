d6dce080e6f84ed489307cf3a395d50d4fb4885c
realdennis <evolon688@gmail.com>
2018-09-12 23:43:36 +0800

    feature: for age 18 board

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+#!/usr/bin/env node
 let pageParser = require('./pageParser');
 let articleParser = require('./articleParser');
 const fs = require('fs');
-const moment = require('moment');
-Date.prototype.toJSON = function() {
-  return moment(this).format();
-};
-
-const Board = 'Beauty';
-let Page = '1869';
-let Count = 1000;
-
 const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
 
+let Board = 'Gossiping';
+let PageNumber = 0;
+let age18 = 1;
+let headlessOption = true;
+if (process.argv[2]) Board = process.argv[2];
+if (process.argv[3]) PageNumber = process.argv[3];
+if (process.argv[4]) headlessOption = process.argv[4];
+console.log(
+  `Board ${Board} / Page ${PageNumber} / headless? ${headlessOption}`
+);
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: headlessOption });
+  const page = await browser.newPage();
   await page.setRequestInterception(true);
   page.on('request', request => {
     if (
@@ -30,44 +33,58 @@ const puppeteer = require('puppeteer');
 
   let pageInfo;
 
-  while (Count--) {
-    try {
-      await page.goto(`https://www.ptt.cc/bbs/${Board}/index${Page}.html`, {
-        waitUntil: 'domcontentloaded',
-        timeout: 50 * 1000
-      });
-      pageInfo = await page.evaluate(pageParser);
-      Page = pageInfo.pageNumber; //Now page;
-      let articleInfo = [];
+  while (true) {
+    let p = await page.goto(`https://www.ptt.cc/bbs/${Board}/index${PageNumber}.html`, {
+      waitUntil: 'domcontentloaded',
+      timeout: 0
+    });
 
-      for (let i = 0; i < pageInfo.links.length; i++) {
-        let article = await page.goto(pageInfo.links[i].link, {
+    if (await p.status() >= 400) {
+      console.log('此頁不存在');
+      continue;
+    }
+    if (age18) {
+      await page.setCookie({
+        name: 'over18',
+        value: '1'
+      });
+      //不可以reload 因為頁面被跳轉了
+      await page.goto(
+        `https://www.ptt.cc/bbs/${Board}/index${PageNumber}.html`,
+        {
           waitUntil: 'domcontentloaded',
           timeout: 50 * 1000
-        });
-        if(article.status() >= 400){
-          continue;
-        }
-        articleInfo.push(await page.evaluate(articleParser));
-      }
-      //write data to json
-      if (!fs.existsSync(`${Board}`)) fs.mkdirSync(`${Board}`);
-
-      let currentPage = Page;
-      fs.writeFile(
-        `${Board}/${Board}_${currentPage}.json`,
-        JSON.stringify(articleInfo),
-        { flag: 'w' },
-        err => {
-          if (err) console.log(err);
-          console.log(`saved as ${Board}/${Board}_${currentPage}.json`);
         }
       );
-      Page -= 1;
-    } catch (e) {
-      console.log(e.message);
-      console.log('這頁出問題ㄌ 重來ㄦ');
+      age18 -= 1; // only one time to reload cookie;
     }
+    //For over18 board
+    pageInfo = await page.evaluate(pageParser);
+    PageNumber = pageInfo.pageNumber; //Now page;
+    let articleInfo = [];
+
+    for (let i = 0; i < pageInfo.links.length; i++) {
+      let article = await page.goto(pageInfo.links[i].link, {
+        waitUntil: 'domcontentloaded',
+        timeout: 0
+      });
+      if (await article.status() >= 400) {
+        console.log('此篇文章不存在');
+        continue;
+      }
+      articleInfo.push(await page.evaluate(articleParser));
+    }
+    //write data to json
+    if (!fs.existsSync(`data/${Board}`)) fs.mkdirSync(`data/${Board}`);
+
+    fs.writeFileSync(
+      `data/${Board}/${Board}_${PageNumber}.json`,
+      JSON.stringify(articleInfo),
+      { flag: 'w' }
+    );
+    console.log(`Saved as data/${Board}/${Board}_${PageNumber}.json`);
+    PageNumber -= 1;
+    if (PageNumber === 0) break;
   }
   await browser.close();
 })();
