4cb02c621ea4be8fd5f93cca21da1dff727c984c
realdennis <evolon688@gmail.com>
2018-10-05 01:01:03 +0800

    docs: update result in README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# ptt-parser 
+
+## Introduction
+使用puppeteer(pptr)撰寫的爬蟲，核心方法是使用DOM操作(querySelector)，再把腳本傳進去pptr跑，讓前端人們輕而一舉的撰寫爬蟲，此repo作為教學/介紹用途，安裝方法請參考以下，或是自行clone下來install。
+
+## Dependency
+Only one -- Puppeteer!
+
+
+## Demo
+![](https://raw.githubusercontent.com/realdennis/ptt-parser/master/demo/demo.jpg)
+
+## Usage
+```
+$  npm install ptt-parser -g
+$  ptt-parser gossiping 1000 false
+    or
+$  ptt-parser beauty //預設從最新一頁爬取
+```
+解釋:
+ptt-parser ${看板名} ${欲往前爬取的頁數} ${是否headless}
+
+## Result
+![](https://raw.githubusercontent.com/realdennis/ptt-parser/master/demo/demo2.jpg)
--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 let Board = 'Gossiping';
-let PageNumber = 0;
+let nowPage = 0;
 let age18 = 1;
-let headlessOption = true;
+let isHeadless = true;
 if (process.argv[2]) Board = process.argv[2];
-if (process.argv[3]) PageNumber = process.argv[3];
-if (process.argv[4]) headlessOption = process.argv[4];
-console.log(
-  `Board ${Board} / Page ${PageNumber} / headless? ${headlessOption}`
-);
+if (process.argv[3]) nowPage = process.argv[3];
+if (process.argv[4] === 'false') isHeadless = false;
+console.log(`Board ${Board} / Page ${nowPage} / headless? ${isHeadless}`);
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: headlessOption });
+  const browser = await puppeteer.launch({ headless: isHeadless });
   const page = await browser.newPage();
   await page.setRequestInterception(true);
   page.on('request', request => {
@@ -35,7 +33,7 @@ console.log(
 
   while (true) {
     let p = await page.goto(
-      `https://www.ptt.cc/bbs/${Board}/index${PageNumber}.html`,
+      `https://www.ptt.cc/bbs/${Board}/index${nowPage}.html`,
       {
         waitUntil: 'domcontentloaded',
         timeout: 0
@@ -52,18 +50,15 @@ console.log(
         value: '1'
       });
       //不可以reload 因為頁面被跳轉了
-      await page.goto(
-        `https://www.ptt.cc/bbs/${Board}/index${PageNumber}.html`,
-        {
-          waitUntil: 'domcontentloaded',
-          timeout: 50 * 1000
-        }
-      );
+      await page.goto(`https://www.ptt.cc/bbs/${Board}/index${nowPage}.html`, {
+        waitUntil: 'domcontentloaded',
+        timeout: 50 * 1000
+      });
       age18 -= 1; // only one time to reload cookie;
     }
     //For over18 board
     pageInfo = await page.evaluate(pageParser);
-    PageNumber = pageInfo.pageNumber; //Now page;
+    nowPage = pageInfo.pageNumber; //Now page;
     let articleInfo = [];
 
     for (let i = 0; i < pageInfo.links.length; i++) {
@@ -77,17 +72,18 @@ console.log(
       }
       articleInfo.push(await page.evaluate(articleParser));
     }
+    if (!fs.existsSync('./data')) fs.mkdirSync('./data');
     //write data to json
-    if (!fs.existsSync(`data/${Board}`)) fs.mkdirSync(`data/${Board}`);
+    if (!fs.existsSync(`./data/${Board}`)) fs.mkdirSync(`./data/${Board}`);
 
     fs.writeFileSync(
-      `data/${Board}/${Board}_${PageNumber}.json`,
+      `./data/${Board}/${Board}_${nowPage}.json`,
       JSON.stringify(articleInfo),
       { flag: 'w' }
     );
-    console.log(`Saved as data/${Board}/${Board}_${PageNumber}.json`);
-    PageNumber -= 1;
-    if (PageNumber === 0) break;
+    console.log(`Saved as data/${Board}/${Board}_${nowPage}.json`);
+    nowPage -= 1;
+    if (nowPage === 0) break;
   }
   await browser.close();
 })();
